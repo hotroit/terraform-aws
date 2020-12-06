81613963e649f290671b5e7a4b36695099671747
hotroit <dungit@hotmail.com>
2020-12-06 15:03:34 +0700

    Create a single webserver

--- example.tf
+++ example.tf
@@ -5,8 +5,25 @@ provider "aws" {
 resource "aws_instance" "example" {
 	ami = "ami-0dd9f0e7df0f0a138"
 	instance_type = "t2.micro"
-
+	vpc_security_group_ids = [aws_security_group.instance.id]
+	
+	user_data = <<-EOF
+				#!/bin/bash
+				echo "Hello, welcome you to the World" > index.html
+				nohub busybox httpd -f -p 8080 &
+				EOF
 	tags = {
 		Name = "terraform-example"
+	}
+}
+
+resource "aws_security_group" "instance" {
+	name = "terraform-example-instance"
+
+	ingress {
+		from_port = 8080
+		to_port = 8080
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
 	}
 }
